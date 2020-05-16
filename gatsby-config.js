a690876d8597c0c176011f097ff39a2aea232185
Tom Konidas <tomkonidas@protonmail.ch>
2020-05-16 15:50:30 -0400

    Add team

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `team`,
+        path: `${__dirname}/src/pages/team/`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {

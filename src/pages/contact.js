8adcd10da382930645f725c0a2ac2a299507ae38
Tom Konidas <tomkonidas@protonmail.ch>
2020-05-16 12:44:23 -0400

    Add contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Contact = () => (
+  <Layout>
+    <SEO title="Get In Touch" />
+    <div>
+      <h1>Get in touch</h1>
+      <h2>Follow us on ALL TO SOCIALS</h2>
+      <p>
+        I'm baby twee bicycle rights pitchfork gentrify taxidermy. Semiotics
+        fingerstache before they sold out activated charcoal. Helvetica portland
+        farm-to-table tumeric twee plaid. Gochujang occupy humblebrag tousled
+        next level brooklyn cornhole tofu. Tumeric hot chicken hell of swag,
+        etsy flexitarian subway tile DIY mixtape normcore gluten-free. Hashtag
+        adaptogen mustache air plant flexitarian street art enamel pin messenger
+        bag dreamcatcher succulents tattooed vegan everyday carry woke.
+      </p>
+    </div>
+  </Layout>
+)
+
+export default Contact

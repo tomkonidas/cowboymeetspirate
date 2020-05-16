4b969570a1b2806aea87ff92a6efdbc7670bacba
Tom Konidas <tomkonidas@protonmail.ch>
2020-05-16 19:06:19 -0400

    add styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,9 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+import cmpBand from "../images/cmp-band.jpg"
 
 const IndexPage = () => (
   <Layout>
@@ -11,8 +12,14 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div>
+      <img
+        style={{
+          width: `100%`,
+        }}
+        src={cmpBand}
+        alt="Cowboy Meets Pirate"
+      />
     </div>
     <Link to="/about/">About</Link>
     <Link to="/contact/">Contact</Link>

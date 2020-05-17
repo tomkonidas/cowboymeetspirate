92c780541092ead6a4cd6c4662150bc79559a5d9
Tom Konidas <tomkonidas@protonmail.ch>
2020-05-17 12:50:13 -0400

    add css normalize and defaults

---
+++ src/theme/globalStyles.js
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from "styled-components"
+
+import "normalize.css"
+
+const GlobalStyle = createGlobalStyle`
+    html {
+        height: 100%;
+        font-size: 1px;
+    }
+
+    *,
+    *::before,
+    *::after{box-sizing:border-box;}
+    a{text-decoration:none; color:inherit; cursor:pointer;}
+    button{background-color:transparent; color:inherit; border-width:0; padding:0; cursor:pointer;}
+    figure{margin:0;}
+    input::-moz-focus-inner {border:0; padding:0; margin:0;}
+    ul, ol, dd{margin:0; padding:0; list-style:none;}
+    h1, h2, h3, h4, h5, h6{margin:0; font-size:inherit; font-weight:inherit;}
+    p{margin:0;}
+    cite {font-style:normal;}
+    fieldset{border-width:0; padding:0; margin:0;}
+
+    body{
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+        font-size: 16rem;
+        font-weight: 400;
+        line-height: 1.3;
+        color: #222;
+    }
+
+
+`
